00d44f156f3ec565ba2c44aab6a837e12ae82c5c
ivan <ivanjose_04@hotmail.com>
2024-05-09 22:14:45 -0400

    first commit

---
+++ prjWebCsEntityFrameworkAdo/webSportEF.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webSportEF.aspx.cs" Inherits="prjWebCsEntityFrameworkAdo.webSportEF" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript">
+        function valider() {
+            return window.confirm("Etes-vous sur de vouloir supprimer cette equipe ?");
+        }
+    </script>
+<style type="text/css">
+    table{
+        margin:auto;
+        width:700px;
+        border-radius:10px;
+        padding:2px;
+        border-spacing:4px;
+        background-color:bisque;
+        font-weight:bold;
+        color:saddlebrown;
+    }
+     .auto-style1 {
+     width: 100%;
+     border-style: solid;
+     border-width: 1px;
+ }
+
+</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 style="text-align:center">ENTITY FRAMEWORK Gestion Equipes Sportives</h1>
+            <hr style="width:600px" />
+
+            <table>
+                <tr>
+                    <td>
+                        Choisir une equipe<br />
+                        <asp:ListBox ID="lstEquipes"  runat="server" AutoPostBack="True" Font-Bold="True" ForeColor="Blue" Height="100" Width="200" OnSelectedIndexChanged="lstEquipes_SelectedIndexChanged"></asp:ListBox>
+                    </td>
+                    <td>
+
+                        <table class="auto-style1">
+    <tr>
+        <td colspan="2">Gestion de equipe selectionnee</td>
+       
+        <td rowspan="5">
+            <asp:Button ID="btnAjouter" OnClick="btnAjouter_Click" runat="server" Text="Ajouter" Font-Bold="True" ForeColor="White" BackColor="Brown" Width="150px" /><br />
+            <asp:Button ID="btnModifier" OnClick="btnModifier_Click" runat ="server" Text="Modifier" Font-Bold="True" ForeColor="White" BackColor="Brown" Width="150px" /><br />
+            <asp:Button ID="btnSupprimer" OnClick="btnSupprimer_Click" runat="server" OnClientClick="return valider();" Text="Supprimer" Font-Bold="True" ForeColor="White" BackColor="Brown" Width="150px" /><br />
+            <asp:Button ID="btnSauvgarder" OnClick="btnSauvgarder_Click" runat="server" Text="Sauvgarder" Font-Bold="True" ForeColor="White" BackColor="Brown" Width="150px" /><br />
+            <asp:Button ID="btnAnnuler" OnClick="btnAnnuler_Click" runat="server" Text="Annuler" Font-Bold="True" ForeColor="White" BackColor="Brown" Width="150px" />
+        </td>
+    </tr>
+    <tr>
+        <td>Nom : </td>
+        <td>
+            <asp:TextBox ID="txtNom" runat="server" Font-Bold="True" ForeColor="Blue" Width="200px"></asp:TextBox>
+
+
+        </td>
+       
+    </tr>
+    <tr>
+        <td>Ville : </td>
+        <td><asp:TextBox ID="txtVille" runat="server" Font-Bold="True" ForeColor="Blue" Width="200px"></asp:TextBox>
+
+
+        </td>
+        
+    </tr>
+    <tr>
+        <td>Budget : </td>
+        <td>
+            <asp:TextBox ID="txtBudget" runat="server" Font-Bold="True" ForeColor="Blue" Width="200px"></asp:TextBox>
+        </td>
+        
+    </tr>
+    <tr>
+        <td>Coach : </td>
+        <td>
+            <asp:TextBox ID="txtCoach" runat="server" Font-Bold="True" ForeColor="Blue" Width="200px"></asp:TextBox>
+        </td>
+        
+    </tr>
+</table>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:GridView ID="gridJoueurs" runat="server" BackColor="#66FFFF" BorderStyle="Solid" BorderWidth="2" Font-Bold="True" ForeColor="#663300" Width="100%"></asp:GridView>
+                    </td>
+                </tr>
+
+            </table>
+
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ prjWebCsEntityFrameworkAdo/webSportEF.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prjWebCsEntityFrameworkAdo
+{
+    public partial class webSportEF : System.Web.UI.Page
+    {
+        //variables globales
+        static DBSportEntities1 myEFSport;
+        static string mode;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack == false)
+            {
+                myEFSport = new DBSportEntities1();
+                RemplirListeEquipes();
+            }
+        }
+
+        private void RemplirListeEquipes()
+        {
+            ////version boucle avec EF
+            //foreach(Equipe eq in myEFSport.Equipes)
+            //{
+            //    ListItem elm = new ListItem();
+            //    elm.Text = eq.Nom;
+            //    elm.Value = eq.RefEquipe.ToString();
+            //    lstEquipes.Items.Add(elm);
+            //}
+
+
+            ////version databinding avec EF
+            //lstEquipes.DataTextField = "Nom";
+            //lstEquipes.DataValueField = "RefEquipe";
+            //lstEquipes.DataSource = myEFSport.Equipes.ToList();
+            //lstEquipes.DataBind();
+
+
+
+            //version linq avec EF
+            var lesEquipes = from Equipe eq in myEFSport.Equipes
+                             select new
+                             {
+                                 Nom = eq.Nom,
+                                 RefEquipe = eq.RefEquipe,
+                             };
+            lstEquipes.DataTextField = "Nom";
+            lstEquipes.DataValueField = "RefEquipe";
+            lstEquipes.DataSource = lesEquipes.ToList();
+            lstEquipes.DataBind();
+        }
+
+        protected void lstEquipes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //recuperer ref equipe de l'equipe choisi
+            string refEqpChoisi = lstEquipes.SelectedItem.Value;
+            //version Boucle avec EF
+            //foreach (Equipe eq in myEFSport.Equipes)
+            //{
+            //    if (eq.RefEquipe.ToString() == refEqpChoisi)
+            //    {
+            //        txtNom.Text = eq.Nom;
+            //        txtVille.Text = eq.Ville;
+            //        txtCoach.Text = eq.Coach;
+            //        txtBudget.Text = eq.Budget.ToString();
+
+            //        //afficher les joueurs de cette equipe dans le grid 
+            //        gridJoueurs.DataSource = eq.Joueurs.ToList();
+            //        gridJoueurs.DataBind();
+
+            //        break;
+            //    }
+            //}
+
+
+            //version EF avec la methode find()
+            Equipe eq = myEFSport.Equipes.Find(Convert.ToInt32(refEqpChoisi));
+            txtNom.Text = eq.Nom;
+            txtVille.Text = eq.Ville;
+            txtCoach.Text = eq.Coach;
+            txtBudget.Text = eq.Budget.ToString();
+            //afficher les joueurs de cette equipe dans le grid 
+            gridJoueurs.DataSource = eq.Joueurs.ToList();
+            gridJoueurs.DataBind();
+
+        }
+
+        protected void btnAjouter_Click(object sender, EventArgs e)
+        {
+            mode = "ajout";
+            txtBudget.Text = txtCoach.Text = txtVille.Text = txtNom.Text = "";
+            txtNom.Focus();
+        }
+
+        protected void btnModifier_Click(object sender, EventArgs e)
+        {
+            mode = "modif";
+
+            txtNom.Focus();
+        }
+
+        protected void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            if (lstEquipes.SelectedItem == null)
+            {
+                return;
+            }
+
+            int refEqpChoisi = Convert.ToInt32(lstEquipes.SelectedItem.Value);
+
+            Equipe equipeASupprimer = myEFSport.Equipes.Find(refEqpChoisi);
+
+            if (equipeASupprimer != null)
+            {
+                myEFSport.Equipes.Remove(equipeASupprimer);
+
+                myEFSport.SaveChanges();
+
+                RemplirListeEquipes();
+
+                ClearFields();
+            }
+            else
+            {
+                // message ici
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtNom.Text = "";
+            txtVille.Text = "";
+            txtCoach.Text = "";
+            txtBudget.Text = "";
+            gridJoueurs.DataSource = null;
+            gridJoueurs.DataBind();
+        }
+
+
+        protected void btnSauvgarder_Click(object sender, EventArgs e)
+        {
+            if(mode == "ajout")
+            {
+                //creer lequipe
+                Equipe nouvelle = new Equipe();
+                //recuperer les champs
+                nouvelle.Nom = txtNom.Text;
+                nouvelle.Ville = txtVille.Text;
+                nouvelle.Budget = Convert.ToDecimal(txtBudget.Text);
+                nouvelle.Coach = txtCoach.Text;
+                //ajouter lequipe a equipes 
+                myEFSport.Equipes.Add(nouvelle);
+                //ajouter a la bdd
+                myEFSport.SaveChanges();
+                RemplirListeEquipes();
+            }
+            else if (mode == "modif")
+            {
+                int refEqpChoisi = Convert.ToInt32(lstEquipes.SelectedItem.Value);
+
+                Equipe equipeAModifier = myEFSport.Equipes.Find(refEqpChoisi);
+
+                if (equipeAModifier != null)
+                {
+                    equipeAModifier.Nom = txtNom.Text;
+                    equipeAModifier.Ville = txtVille.Text;
+                    equipeAModifier.Budget = Convert.ToDecimal(txtBudget.Text);
+                    equipeAModifier.Coach = txtCoach.Text;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            myEFSport.SaveChanges();
+
+            RemplirListeEquipes();
+
+            ClearFields();
+
+            mode = string.Empty;
+        }
+
+        protected void btnAnnuler_Click(object sender, EventArgs e)
+        {
+            txtNom.Text = "";
+            txtVille.Text = "";
+            txtCoach.Text = "";
+            txtBudget.Text = "";
+
+            lstEquipes.SelectedIndex = -1;
+
+            gridJoueurs.DataSource = null;
+            gridJoueurs.DataBind();
+        }
+    }
+}
